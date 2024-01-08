43f2d25483acd6c9f2c0aea471d48b4a0f721e7f
Sneh Shah <120238003+Sneh-T-Shah@users.noreply.github.com>
2024-01-08 12:46:18 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import os
 from youtube import video_pred
 from image import image_pred
 from image_format_conv import check_and_convert_image
+import numpy as np
 app = Flask(__name__)
 
 ALLOWED_VIDEO_EXTENSIONS = {'mp4'}
@@ -20,6 +21,10 @@ def process_image():
         return jsonify({'error': 'No image file uploaded'}), 400
 
     image = request.files['image']
+    model = request.files['model']
+    dataset = request.files['dataset']
+    threshold = request.files['threshold']
+    
 
     if image.filename == '':
         return jsonify({'error': 'No selected image file'}), 400
@@ -34,8 +39,8 @@ def process_image():
         # print(type(image_path))
         image.save(image_path)
         image_path=check_and_convert_image(image_path)
-        output_string = image_pred(image_path=image_path)
-        return jsonify({'output': output_string}), 200
+        output_string,pred = image_pred(image_path=image_path,model=model,dataset=dataset,threshold=threshold)
+        return jsonify({'output': output_string,'prob':pred}), 200
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -53,6 +58,10 @@ def process_video():
         return jsonify({'error': 'No video file uploaded'}), 400
 
     video = request.files['video']
+    frames = int(request.form.get('frames'))
+    model = request.form.get('model')
+    dataset = request.form.get('dataset')
+    threshold = float(request.form.get('threshold'))
 
     if video.filename == '':
         return jsonify({'error': 'No selected video file'}), 400
@@ -64,9 +73,10 @@ def process_video():
         # Store the video temporarily
         video_path = werkzeug.utils.secure_filename(video.filename)
         video.save(video_path)
-        ans = video_pred(video_path=video_path)
+        ans,pred = video_pred(video_path=video_path,model=model,dataset=dataset,threshold=threshold,frames=frames)
+        print(ans,pred)
 
-        return jsonify({'output': ans}), 200
+        return jsonify({'output': ans,'prob':float(pred)}), 200
 
     except Exception as e:
         # Handle any errors during processing
